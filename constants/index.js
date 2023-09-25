0065f4373f253ef1af5fb76b8fbdee18b543cc5d
Siddharth Movaliya <siddraj1992@gmail.com>
2023-09-25 18:41:16 +0530

    add constants for sidebar component

---
+++ constants/index.js
@@ -0,0 +1,22 @@
+export const sidebarLinks = [
+  {
+    imgURL: "/assets/icons/home.svg",
+    route: "/dashboard",
+    label: "Home",
+  },
+  {
+    imgURL: "/assets/icons/search.svg",
+    route: "/search",
+    label: "Search",
+  },
+  {
+    imgURL: "/assets/icons/heart.svg",
+    route: "/activity",
+    label: "Activity",
+  },
+  {
+    imgURL: "/assets/icons/user.svg",
+    route: "/profile",
+    label: "Profile",
+  },
+];
